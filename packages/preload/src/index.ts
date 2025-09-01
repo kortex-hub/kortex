077c317cc286023977f88dd709fb66437c3a08d3
Florent Benoit <fbenoit@redhat.com>
2025-09-01 22:54:34 +0200

    feat: allow to remove mcp instances
    fixes https://github.com/kortex-hub/kortex/issues/147
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/preload/src/index.ts
+++ packages/preload/src/index.ts
@@ -1713,6 +1713,10 @@ export function initExposure(): void {
     return ipcInvoke('mcp-manager:fetchMcpRemoteServers');
   });
 
+  contextBridge.exposeInMainWorld('removeMcpRemoteServer', async (serverId: string): Promise<void> => {
+    return ipcInvoke('mcp-manager:removeMcpRemoteServer', serverId);
+  });
+
   contextBridge.exposeInMainWorld(
     'unregisterMCPRegistry',
     async (registry: containerDesktopAPI.MCPRegistry): Promise<void> => {
