baa6fa355e6f9555eb53ef86b7a6965ed2e60db3
Simon Rey <51708585+eqqe@users.noreply.github.com>
2025-09-02 14:23:03 +0200

    fix(GooseRecipe): choose model from recipe for kubernetes deploy, refactor (#204)
    * fix(GooseRecipe): choose model from recipe for kubernetes deploy, refactor
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    * refactor: rename
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    
    ---------
    
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>

--- packages/preload/src/index.ts
+++ packages/preload/src/index.ts
@@ -352,11 +352,6 @@ export function initExposure(): void {
   contextBridge.exposeInMainWorld(
     'flowDeployKubernetes',
     async (
-      inference: {
-        providerId: string;
-        connectionName: string;
-        model: string;
-      },
       flow: {
         providerId: string;
         connectionName: string;
@@ -368,7 +363,7 @@ export function initExposure(): void {
         dryrun: boolean;
       },
     ): Promise<string> => {
-      return ipcInvoke('flows:deploy:kubernetes', inference, flow, options);
+      return ipcInvoke('flows:deploy:kubernetes', flow, options);
     },
   );
 
