1a13dc676b27d43a5c13b9876ade907976fd67f9
Jeff MAURY <jmaury@redhat.com>
2025-09-02 11:53:33 +0200

    fix: pass model credentials to Goose (#183)

--- packages/main/src/plugin/provider-registry.ts
+++ packages/main/src/plugin/provider-registry.ts
@@ -38,6 +38,7 @@ import type {
   ProviderContainerConnection,
   ProviderDetectionCheck,
   ProviderEvent,
+  ProviderInferenceConnection,
   ProviderInformation,
   ProviderInstallation,
   ProviderLifecycle,
@@ -1663,6 +1664,19 @@ export class ProviderRegistry {
       this.inferenceProviders.delete(id);
       this.apiSender.send('provider-change', {});
     });
+  }
+
+  getInferenceConnections(): ProviderInferenceConnection[] {
+    const connections: ProviderInferenceConnection[] = [];
+    this.providers.forEach(provider => {
+      provider.inferenceConnections.forEach(connection => {
+        connections.push({
+          providerId: provider.id,
+          connection,
+        });
+      });
+    });
+    return connections;
   }
 
   registerFlowConnection(provider: Provider, connection: FlowProviderConnection): Disposable {
