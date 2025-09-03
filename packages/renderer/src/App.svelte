46f23ab8142d1b629990fca71e74107042192c76
Jeff MAURY <jmauy@redhat.com>
2025-09-03 10:45:35 +0200

    fix: redirect to READY MCPs after MCP has been added
    Fixes #243
    
    Signed-off-by: Jeff MAURY <jmauy@redhat.com>

--- packages/renderer/src/App.svelte
+++ packages/renderer/src/App.svelte
@@ -179,8 +179,8 @@ window.events?.receive('kubernetes-navigation', (args: unknown) => {
         </Route>
 
         <!-- MCP -->
-        <Route path="/mcps" breadcrumb="MCPs" navigationHint="root">
-          <McpServerList />
+        <Route path="/mcps" breadcrumb="MCPs" navigationHint="root" let:meta>
+          <McpServerList tab="{meta.query.tab}"/>
         </Route>
         <Route path="/mcps/:id/*" let:meta>
           <MCPDetails id={meta.params.id} />
