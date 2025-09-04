901d0612aacd90ba8c6cbbd25c37f353d689e648
Simon Rey <sfbrey+eqqe@gmail.com>
2025-09-04 12:15:31 +0200

    fix(chat): reuse export icon in flow create, align
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>

---
+++ packages/renderer/src/lib/chat/components/messages/ExportIcon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { faFileExport } from '@fortawesome/free-solid-svg-icons/faFileExport';
+import Fa from 'svelte-fa';
+
+let {
+  scale,
+}: {
+  scale: number;
+} = $props();
+</script>
+
+<Fa icon={faFileExport} scale={scale}/>
