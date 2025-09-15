2ee0b3cd237f51a6920aaca545a8b5e93bea02f4
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-09-15 17:38:43 +0200

    feat(mcp): init mcp-spawn package
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

---
+++ packages/mcp-spawn/src/npm-spawner.ts
@@ -0,0 +1,58 @@
+/**********************************************************************
+ * Copyright (C) 2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js';
+import type { Transport } from '@modelcontextprotocol/sdk/shared/transport.js';
+
+import { MCPSpawner } from './mcp-spawner';
+
+const NPX_COMMAND = 'npx';
+
+export class NPMSpawner extends MCPSpawner<'npm'> {
+  #disposables: Array<AsyncDisposable> = [];
+
+  async spawn(): Promise<Transport> {
+    if (!this.pack.identifier) throw new Error('missing identifier in MCP Local Server configuration');
+    if (this.pack.file_sha256) {
+      console.warn('specified file sha256 is not supported with npx spawner');
+    }
+
+    // build arguments
+    const RUNTIME_ARGS: Array<string> = (this.pack.runtime_arguments ?? []).map(this.getArgument.bind(this));
+    const PACKAGE_ARGS: Array<string> = (this.pack.package_arguments ?? []).map(this.getArgument.bind(this));
+
+    const transport = new StdioClientTransport({
+      command: NPX_COMMAND,
+      args: [
+        ...RUNTIME_ARGS,
+        // let's use package@version if version is specified
+        this.pack.version ? `${this.pack.identifier}@${this.pack.version}` : this.pack.identifier,
+        ...PACKAGE_ARGS,
+      ],
+    });
+    this.#disposables.push({
+      [Symbol.asyncDispose]: () => {
+        return transport.close();
+      },
+    });
+    return transport;
+  }
+
+  async [Symbol.asyncDispose](): Promise<void> {
+    await Promise.allSettled(this.#disposables.map(disposable => disposable[Symbol.asyncDispose]()));
+  }
+}
