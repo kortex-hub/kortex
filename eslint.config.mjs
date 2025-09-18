06bb776cb0f4da30cc058cac2b8a957dc6a2b5c1
Florent Benoit <fbenoit@redhat.com>
2025-09-18 18:48:51 +0200

    chore: apply format:fix
    new files were broken
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -77,7 +77,7 @@ export default [
       'tests/playwright/scripts/**',
       'website/storybook.ts',
       'website/src/pages/storybook/sidebar.cjs',
-      'packages/mcp-registry/**/*'
+      'packages/mcp-registry/**/*',
     ],
   },
   js.configs.recommended,
