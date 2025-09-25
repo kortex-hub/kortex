f5913ec78e4b219b03725beb7d6c0f466748fa17
serbangeorge-m <serbangeorge.m@gmail.com>
2025-09-25 17:41:30 +0200

    chore(test): update config
    Signed-off-by: serbangeorge-m <serbangeorge.m@gmail.com>

--- tests/playwright/src/fixtures/electron-app.ts
+++ tests/playwright/src/fixtures/electron-app.ts
@@ -1,3 +1,20 @@
+/**********************************************************************
+ * Copyright (C) 2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -16,9 +33,7 @@ export const test = base.extend<ElectronFixtures>({
   electronApp: async ({}, use) => {
     const electronEnv = {
       ...process.env,
-      NODE_ENV: 'development',
       ELECTRON_IS_DEV: '1',
-      ELECTRON_DISABLE_SANDBOX: '1',
     };
 
     delete (electronEnv as Record<string, string | undefined>).ELECTRON_RUN_AS_NODE;
