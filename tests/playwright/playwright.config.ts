790c5c00dc1f9388ef7c4a9ba854669f5a8e588b
serbangeorge-m <serbangeorge.m@gmail.com>
2025-09-25 17:33:15 +0200

    chore(test): update playwright config
    Signed-off-by: serbangeorge-m <serbangeorge.m@gmail.com>

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -1,45 +1,48 @@
+/**********************************************************************
+ * Copyright (C) 2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
 import type { PlaywrightTestConfig } from '@playwright/test';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 const config: PlaywrightTestConfig = {
   testDir: './src',
-  /* Maximum time one test can run for. */
   timeout: 60_000,
-  expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
-    timeout: 10_000,
-  },
-  /* Run tests in files in parallel */
+
   fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
+
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
+
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
+
   workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
   reporter: [
     ['html', { outputFolder: './output/html-results' }],
     ['json', { outputFile: './output/test-results.json' }],
     ['junit', { outputFile: './output/junit-results.xml' }],
     ['list'],
   ],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 10_000,
-    /* Take screenshot when test fails */
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'Electron',
@@ -47,10 +50,7 @@ const config: PlaywrightTestConfig = {
     },
   ],
 
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   outputDir: './output/test-results',
-
-  /* No webServer needed - tests launch Electron directly */
 };
 
 export default config;
