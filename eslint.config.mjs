2bf9d6451e5d4b6135ca65f25487edfaf3c8c60f
Florent Benoit <fbenoit@redhat.com>
2025-09-25 15:29:34 +0200

    chore: switch to npmjs library for mcp registry types
    related to https://github.com/kortex-hub/kortex/issues/454
    
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -77,7 +77,6 @@ export default [
       'tests/playwright/scripts/**',
       'website/storybook.ts',
       'website/src/pages/storybook/sidebar.cjs',
-      'packages/mcp-registry/**/*',
     ],
   },
   js.configs.recommended,
