a70a776d33b84feaf9dd98893188c2691ba61fa7
Jeff MAURY <jmaury@redhat.com>
2025-11-11 09:08:30 +0100

    fix: undo form change
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- packages/renderer/src/lib/chat/components/chat.svelte
+++ packages/renderer/src/lib/chat/components/chat.svelte
@@ -149,7 +149,7 @@ function onCheckMCPTool(mcpId: string, toolId: string, checked: boolean): void {
                     loading={chatClient.status === 'streaming' || chatClient.status === 'submitted'}
                     messages={chatClient.messages}
                 />
-                <form class="bg-background mx-auto flex w-full gap-2 px-4 md:max-w-3xl">
+                <form class="bg-background mx-auto flex w-full gap-2 px-4 pb-4 md:max-w-3xl md:pb-6">
                     {#if !readonly}
                         <MultimodalInput {attachments} {chatClient} {selectedModel} {selectedMCPTools} class="flex-1" />
                     {/if}
