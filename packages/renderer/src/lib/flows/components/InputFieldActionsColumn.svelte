1833de0d2a083bfc9ec9be926311686d990aa07d
Simon Rey <51708585+simonrey1@users.noreply.github.com>
2025-12-10 10:37:52 +0100

    refactor(renderer/flow/params): make parent responsible to give edit and delete methods for each
    Signed-off-by: Simon Rey <51708585+simonrey1@users.noreply.github.com>

--- packages/renderer/src/lib/flows/components/InputFieldActionsColumn.svelte
+++ packages/renderer/src/lib/flows/components/InputFieldActionsColumn.svelte
@@ -1,40 +1,26 @@
 <script lang="ts">
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
-import type { InputField } from '/@/lib/flows/types/input-field';
+import type { ParameterWithCallbacks } from '/@/lib/flows/components/InputFieldTable.svelte';
 import ListItemButtonIcon from '/@/lib/ui/ListItemButtonIcon.svelte';
-
-interface ParameterWithCallbacks extends InputField {
-  index: number;
-  onEdit: (index: number, field: InputField) => void;
-  onDelete: (index: number, field: InputField) => void;
-}
 
 interface Props {
   object: ParameterWithCallbacks;
 }
 
 let { object }: Props = $props();
-
-function handleEdit(): void {
-  object.onEdit(object.index, object);
-}
-
-function handleDelete(): void {
-  object.onDelete(object.index, object);
-}
 </script>
 
 <div class="flex justify-end gap-1">
   <ListItemButtonIcon
     title="Edit parameter"
     icon={faEdit}
-    onClick={handleEdit}
+    onClick={object.onEdit}
   />
   <ListItemButtonIcon
     title="Delete parameter"
     icon={faTrash}
-    onClick={handleDelete}
+    onClick={object.onDelete}
   />
 </div>
 
