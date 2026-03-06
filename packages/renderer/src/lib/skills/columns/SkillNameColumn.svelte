b73a446c99b5e803d39bff7c09c2e3724d09682f
Evzen Gasta <evzen.ml@seznam.cz>
2026-03-06 21:42:06 +0100

    feat(Skills list): added page to display skills
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

---
+++ packages/renderer/src/lib/skills/columns/SkillNameColumn.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+import type { SkillInfo } from '/@api/skill/skill-info';
+
+interface Props {
+  object: SkillInfo;
+}
+
+let { object }: Props = $props();
+</script>
+
+<div class="flex flex-col whitespace-nowrap max-w-full">
+  <div class="flex items-center max-w-full">
+    <div class="max-w-full">
+      <div class="flex flex-nowrap max-w-full">
+        <div
+          class="text-[var(--pd-table-body-text-highlight)] overflow-hidden text-ellipsis"
+          title={object.name}>
+          {object.name}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
